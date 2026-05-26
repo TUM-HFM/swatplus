f7dc035ea4675923c5c92182f104653b91c4a04b
Moritz Wirthensohn <wirthensohn.moritz@gmail.com>
2026-05-26 08:06:46 +0200

    wallo_withdraw: allow exact maximum channel withdrawal
    Strict < prevented withdrawing exactly the available flow (e.g. all_flo
    on a day where available equals demand). Changed to <= so that withdrawal
    succeeds when demand equals cha_div.

--- src/wallo_withdraw.f90
+++ src/wallo_withdraw.f90
@@ -41,7 +41,7 @@
         cha_min = wallo(iwallo)%src(isrc_wallo)%limit_mon(time%mo) * 86400.  !m3 = m3/s * 86400s/d
         !! amount that can be diverted without falling below low flow limit
         cha_div = ht2%flo - cha_min
-        if (dmd_m3 < cha_div) then
+        if (dmd_m3 <= cha_div) then
           rto = dmd_m3 / ht2%flo
           ht5 = rto * ht2
           ht2 = (1. - rto) * ht2
